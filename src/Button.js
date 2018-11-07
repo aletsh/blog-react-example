727486c5dc0630a8968edeb62f78785d68e2f71f
Alejandro Guerrero Ploeg <aleguerrero@hotelbeds.com>
2018-11-07 18:28:32 +0100

    New param to select a button type

--- src/Button.js
+++ src/Button.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 
 export default class Button extends React.Component {
+
     render() {
+        // if is not a link, this is a button ^^
+        let classbutton = this.props.button_type == "link" ? "text-secondary" : "btn btn-primary";
+        
         return (
-            <Link class="btn btn-primary" to={`/post/${this.props.post_id}`}>{this.props.button_text}</Link>
+            <Link class={classbutton} to={`/post/${this.props.post_id}`}>{this.props.button_text}</Link>
         );
     }
 }
